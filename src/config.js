a3517d2ccf4cda3bc8c420ff7b5d9840c64c4744
Sickora, Ross <Sickora.Ross@principal.com>
2019-06-10 15:30:34 -0500

    added aws amplify with cognito identity mgmt

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+  amplify: {
+    Auth: {
+      identityPoolId: 'us-east-1:03e3004d-6798-4ded-a916-7996c4083064',
+      region: 'us-east-1',
+      userPoolId: 'us-east-1_4zudL72x1',
+      userPoolWebClientId: '73top014i8412pu9169ul2d6rh',
+    },
+    Analytics: {
+      disabled: true,
+    },
+    aws_appsync_graphqlEndpoint: 'URL',
+    aws_appsync_region: 'us-east-1',
+    aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+      
+    
+  }
+}
