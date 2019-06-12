c00f0e5b71593d26c92218cf48a54440d54b3a87
Sickora, Ross <Sickora.Ross@principal.com>
2019-06-12 09:07:39 -0500

    adding react-test-renderer

--- src/components/Market.test.js
+++ src/components/Market.test.js
@@ -7,3 +7,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<Market />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders an App Bar on the screen', () => {
+  const div = document.createElement('div');
+  ReactDom.render(<Market />, div);
+  
+});
