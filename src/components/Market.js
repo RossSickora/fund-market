182157b399a1944c594b25a32dabbe37a73d3af2
Sickora, Ross <Sickora.Ross@principal.com>
2019-06-26 14:14:09 -0500

    updating market to be a component

--- src/components/Market.js
+++ src/components/Market.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 //import ReactDom from 'react-dom';
 
 import AppBar from './shared/AppBar'
 
-function Market() {
-  return (
-    <div className="Market">
-      <AppBar />
-      <h1>Hello, world!</h1>
-    </div>
-  );
+class Market extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {user: undefined}
+  }
+  render() {
+    return (
+      <div className="Market">
+        <AppBar />
+        <h1>Hello, world!</h1>
+      </div>
+    );
+  }
 }
+
 
 export default Market;
