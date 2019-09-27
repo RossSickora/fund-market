40344aa8d05ef2ee106851743ae41f95d0124ee8
Ross Sickora <r.sickora@gmail.com>
2019-09-27 11:34:37 -0500

    shipping it

--- src/components/shared/FundItem.test.js
+++ src/components/shared/FundItem.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils'
+import renderer from 'react-test-renderer';
 import FundItem from './FundItem';
 
 it('renders without crashing', () => {
@@ -7,3 +9,20 @@ it('renders without crashing', () => {
   ReactDOM.render(<FundItem />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// it('changes the elevation to 6 on mouse entering the component', (done) => {
+//     const component = renderer.create(<FundItem/>,);
+
+//     let tree = component.toJSON();
+//     expect(tree).toMatchSnapshot();
+
+
+//     tree.props.onMouseOver();
+//     tree = component.toJSON();
+//     //expect(tree.children.elevation).toEqual(6);
+//     expect(tree).toMatchSnapshot();
+
+//     tree.props.onMouseOut();
+//     tree = component.toJSON();
+//     expect(tree).toMatchSnapshot();
+// });
